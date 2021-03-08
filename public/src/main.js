4a43bbc85b749a1ca51419babc64ce377373f316
LinaRietuma <lina.rietuma@gmail.com>
2021-03-09 01:26:51 +0200

    Import .js files

---
+++ public/src/main.js
@@ -0,0 +1,10 @@
+import {name, dropdown, getSelectedOption} from './dropdown.js';
+import {graph} from './graph.js';
+
+window.addEventListener('load', (event) => {
+  dropdown("dropdown-year");
+  dropdown("dropdown-data");
+  graph();
+});
+
+window.update
