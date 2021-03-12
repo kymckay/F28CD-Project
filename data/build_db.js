14381f375071adb374655b79f42fd3b8093a7436
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-12 17:34:37 +0000

    Output candidates with no voting data during join

--- data/build_db.js
+++ data/build_db.js
@@ -42,10 +42,17 @@ async function main() {
   candidates.forEach(c => {
     const { parties: partyVotes } = electionData[c.year][c.gss_code];
 
-    // If candidate party is not registered, they fall under other
+    // If candidate party is not recognised, they fall under other
     // TODO: Independent candidate votes are all bundled under "Other" in the data and can't be seperated
-    // TODO generate prediction data too (try to make consistent with electorate)
-    c.votes = (c.party_ec_id in partyVotes) ? partyVotes[c.party_ec_id] : partyVotes.Other;
+    c.votes = partyVotes[c.party_ec_id] ? partyVotes[c.party_ec_id] : partyVotes.Other;
+
+    // If Other has no voting data for the region then the candidate's party is not reflected in the voting data
+    if (!c.votes) {
+      c.votes = -1;
+      console.log(`Warning: No voting data for party "${c.party_ec_id}" in region "${c.gss_code}" of year ${c.year}`);
+    }
+
+    // TODO: generate prediction data too (try to make consistent with electorate of region)
   });
 
   console.log(`Inserting data into MongoDB...`);
