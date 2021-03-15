f19a0978b71eb9620074bc49433cfb2318da682e
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-15 15:29:10 +0000

    Update candSect.js

--- public/src/candSect.js
+++ public/src/candSect.js
@@ -1,27 +1,20 @@
 const ctx = document.getElementById('candGraph').getContext('2d');
 const names = ['First Place', 'Second Place'];
+const data = [100000, 90000]
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: names,
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: data,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#0d324d',
+                '#7f5a83'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#0d324',
+                '#7f5a83'
             ],
             borderWidth: 1
         }]
