1d990b4832ee210f32b898040d1d41342754ed85
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-16 17:49:01 +0000

    Convert search script to ES6 syntax

--- public/src/search.js
+++ public/src/search.js
@@ -1,13 +1,13 @@
 function search() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("candid");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+  const input = document.getElementById("search");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("candid");
+  const tr = table.getElementsByTagName("tr");
+
+  for (const i in tr) {
+    const td = tr[i].getElementsByTagName("td")[0];
     if (td) {
-      txtValue = td.textContent || td.innerText;
+      const txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
