520f204190a45497e49d6304e33ac80372b588af
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-17 17:34:21 +0000

    Encapsulate dropdown functionality within module

---
+++ public/src/dropdowns.js
@@ -0,0 +1,31 @@
+import { getYear } from './requests.js';
+
+// Populates a dropdown (identified by ID in HTML) with array of values
+function populateDropdown(id, data) {
+  // First clear any existing options
+  const dd = document.getElementById(id);
+  dd.innerHTML = '';
+
+  // Populate the dropdown with provided options
+  data.forEach(val => {
+    const opt = document.createElement("option");
+    opt.innerHTML = val;
+    opt.setAttribute("value", val);
+    dd.appendChild(opt);
+  });
+}
+
+export function populateDropdowns(years, sources) {
+  populateDropdown('dropdown-year', years);
+  populateDropdown('dropdown-data', sources);
+}
+
+
+// Adds event handling to drop down elements
+export function initDropdowns() {
+  // Handle updates on year change
+  document.getElementById("dropdown-year").addEventListener("change", e => getYear(e.target.value));
+
+  // Handle updates on source change
+  document.getElementById("dropdown-data").addEventListener("change", e => { console.log(e.target.value) })
+}
--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,6 @@
-import { getYears, getYear } from './requests.js';
+import { getYears } from './requests.js';
 import { initMap } from './map.js';
-import { newDropdown } from './dropdown.js';
+import { initDropdowns, populateDropdowns } from './dropdowns.js';
 import { graph } from './graph.js';
 import { candidateGraph } from './candSect.js';
 import { populateList, initSearch } from './list.js';
@@ -11,28 +11,17 @@ const MAPBOX_KEY = '';
 
 // Element functionality initalised here
 function initPage() {
+  initMap(MAPBOX_KEY);
+  initDropdowns();
   enableSort();
   initSearch();
   candidateGraph();
   graph();
-  initMap(MAPBOX_KEY);
-}
-
-// Dropdowns need data to initalise with
-function initDropdowns(data) {
-  // Populate year dropdown and handle updates on year change
-  const yearSel = newDropdown("dropdown-year", data.years);
-  yearSel.addEventListener("change", e => getYear(e.target.value));
-
-
-  const srcSel = newDropdown("dropdown-data", data.sources);
-
-  // Handle updates on source change
-  srcSel.addEventListener("change", e => { console.log(e.target.value) })
 }
 
 // Populates elements whenever a new year's data is retrieved
 function populatePage(data) {
+  populateDropdowns(data.years, data.sources);
   populateList(data);
 }
 
@@ -40,15 +29,10 @@ document.addEventListener('DOMContentLoaded', initPage);
 
 // Immediately send a request off for the initial data needed to populate the page
 getYears().then((data) => {
-  function init() {
-    initDropdowns(data);
-    populatePage(data);
-  }
-
   // Populate elements once the DOM is ready (or immediately if already)
   if (document.readyState !== 'loading') {
-    init();
+    populatePage(data);
   } else {
-    document.addEventListener('DOMContentLoaded', init);
+    document.addEventListener('DOMContentLoaded', () => populatePage(data));
   }
 });
