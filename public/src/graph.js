374e71d8df03134ce15de6fb14468370b60dd575
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-20 22:47:29 +0000

    Update predictions on graph when source changes
    I have some doubts about the way I've coded this (how it will work once
    constituency specific view is introduced), but they can be dealt with
    when/if problems arise.

--- public/src/graph.js
+++ public/src/graph.js
@@ -3,6 +3,10 @@
 // Chart will be updated later
 let chart;
 
+// Prediction data can be switched between after load
+const predictions = [];
+let predictIndex = 0; // Index should persist when year changes
+
 // Reference: https://www.chartjs.org/docs/latest/
 export async function initGraph() {
   const ctx = document.getElementById('canvas').getContext('2d');
@@ -28,41 +32,61 @@ export async function initGraph() {
   });
 }
 
+function populatePredictions(top6, rest) {
+  const numSources = top6[0].predictions.length;
+
+  // For each source we store the top 6 parties' predictions
+  // The rest are grouped under "Other" at index 6
+  for (let i = 0; i < numSources; i++) {
+    predictions[i] = top6.map(p => p.predictions[i]);
+    predictions[i].push(rest.reduce((acc, cur) => acc + cur.predictions[i], 0));
+  }
+}
+
 export async function populateGraph(data) {
   // Get the most popular 6 parties
   data.sort((a, b) => b.votes - a.votes);
+
   const top6 = data.slice(0,6);
   top6.sort((a, b) => a.party_name.localeCompare(b.party_name));
+
+  const rest = data.slice(6);
 
   chart.data.labels = top6.map(p => p.party_name);
   chart.data.labels.push('Other');
 
   // Cumulate remaining party votes under "Other" entry
   const dataReal = top6.map(p => p.votes);
-  dataReal.push(data.slice(6).reduce((acc, cur) => acc + cur.votes, 0));
+  dataReal.push(rest.reduce((acc, cur) => acc + cur.votes, 0));
 
-  const dataPred = top6.map(p => p.predictions[0]);
-  dataPred.push(data.slice(6).reduce((acc, cur) => acc + cur.predictions[0], 0));
+  populatePredictions(top6, rest);
 
   // Clear existing data
   chart.data.datasets = [];
 
-  // Show real data as a solid bar
-  chart.data.datasets.push({
+  // Show real data as a solid bar (always first element)
+  chart.data.datasets[0] = {
     label: 'Votes',
     borderWidth: 1,
     borderColor: "#3C4750",
     data: dataReal
-  });
+  };
+
+  // Update the prediction data with the current index
+  updatePredictions(predictIndex);
+}
+
+export async function updatePredictions(index) {
+  predictIndex = index;
 
   // Show prediction data as an outline only bar to differentiate
-  chart.data.datasets.push({
+  chart.data.datasets[1] = {
     label: 'Prediction',
     backgroundColor: '#FFF',
     borderWidth: 1,
     borderColor: "#3C4750",
-    data: dataPred
-  });
+    data: predictions[index]
+  };
 
   chart.update();
 }
