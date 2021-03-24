1dc1359f0657f2b380b60d913284fef0d19752f9
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-24 18:32:15 +0000

    Created Checkbox

--- public/src/toggle.js
+++ public/src/toggle.js
@@ -1,14 +1,11 @@
 export async function initToggle() {
     
-    document.getElementById('checkbox').addEventListener('click', function() {
-        let reults;
-        const lab = document.getElementById('Prediction-Label');
-        if (lab === "Actual") {
-            document.getElementById('Prediction-Label').innerHTML = "Predicted";
+    document.getElementById('check-box').addEventListener('change', function() {
+        if (!this.checked) {
+            document.getElementById('predictionLabel').innerHTML = "Predicted";
         } else {
-            document.getElementById('Prediction-Label').innerHTML = "Actual"
+            document.getElementById('predictionLabel').innerHTML = "Actual"
         };
     })
-    
 
 }
